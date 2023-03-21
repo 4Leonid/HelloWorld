29134a1b5d2eee8fb57dcd92bf5c66ed12b4b7cc
Leonid <leo-turko@mail.ru>
2023-03-21 15:40:20 +0300

    Add Exmaple15 file

---
+++ Example15/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i += 1)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return String.Empty;
+}
+
+Console.Writeline(NumbersFor(1, 10));
+Console.Writeline(NumbersRec(1, 10));
+
+
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.Writeline($"{n++} {new String(word)}"); 
+        return;
+    }
+    for (int i = 0; i < alphabet.Length; i += 1)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("abcd", new char[2]);
+*/
